491e7d1b732ab1c11938bfc9f9a6ad37d5c92984
Dan <mail.ordan@gmail.com>
2023-09-30 00:13:43 +0300

    Complete annotation algorithm based on percentage of target nodes in pipeline

--- pipeline_sampling/sibling_sampling.py
+++ pipeline_sampling/sibling_sampling.py
@@ -8,15 +8,15 @@ from utils.data_writer import write_pipeline
 from utils.debugging import logger
 
 
-def get_items_set(index_df: pd.DataFrame, child: PipelineChild) -> t.Set[str]:
-    input_set_id = child["inputSet"]["id"]
-    input_set = index_df.loc[index_df['id'] == input_set_id]
-    print(input_set)
-    return set(input_set['definition'])
+def find_child_set(index: pd.DataFrame, child: PipelineChild) -> t.Set[str]:
+    query = child["inputSet"]["id"]
+    definition = index.loc[index['id'] == query]['definition'].iloc[0]
+    ids = definition[1:-1].split(', ')
+    return set(ids)
 
 
-def annotate(child: PipelineChild, index_df: pd.DataFrame, target_set: t.Set[str]) -> AnnotatedPipelineChild:
-    child_set = get_items_set(index_df, child)
+def annotate(child: PipelineChild, index: pd.DataFrame, target_set: t.Set[str]) -> AnnotatedPipelineChild:
+    child_set = find_child_set(index, child)
     total, overlap = len(target_set), len(child_set.intersection(target_set))
     percentage_of_overlap = (overlap / total) * 100
     annotated_child: AnnotatedPipelineChild = copy.deepcopy(child)
@@ -25,14 +25,18 @@ def annotate(child: PipelineChild, index_df: pd.DataFrame, target_set: t.Set[str
 
 
 if __name__ == "__main__":
-    index_df, target_set = (
+    target_set_name = 'grean-peas'
+    index, target_set = (
         read_index(),
-        read_target_set('grean-peas')
+        read_target_set(target_set_name)
     )
-    logger.info("Annotating pipelines with {0} target set".format(target_set))
+    logger.info("Annotating pipelines with target set '{0}' having {1} elements".format(
+        target_set_name,
+        len(target_set)
+    ))
     for uuid, parent, children in read_pipelines():
         annotated_children = list(
-            reversed([annotate(child, index_df, target_set) for child in children[::-1]]))
+            reversed([annotate(child, index, target_set) for child in children[::-1]]))
         write_pipeline(uuid, parent, annotated_children,
                        sampling_method="sibling_sampling")
-    logger.info("Annotation is done as saved!")
+    logger.info("Annotation is done and saved!")
